dd193c000edeee780d9312bfb60c6a2cfda7c772
Zupi-HR <49796750+Zupi-HR@users.noreply.github.com>
2022-10-07 15:45:02 +0200

    added random color generator

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const mainContainer = document.querySelector('.main-container');
 
 function randomColorGenerator() {
-   
+  let color = "#";
+  let colorRange = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+  for(i = 0; i < 6; i++) {
+    color += colorRange[Math.floor(Math.random() * colorRange.length)];
+  }
+  return color;
 }
 
 for (i = 1; i <= 16; i++) {
@@ -12,7 +17,7 @@ for (i = 1; i <= 16; i++) {
         container.appendChild(grid);
         grid.classList.add('grid');
         grid.addEventListener("mouseenter", (e) => {
-            e.target.style.backgroundColor = "purple";
+            e.target.style.backgroundColor = randomColorGenerator();
         })
     }
 }
