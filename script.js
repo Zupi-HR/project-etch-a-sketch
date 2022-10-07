e96b3c04c5a8cb3fa020377cb5424b23b3420344
Zupi-HR <49796750+Zupi-HR@users.noreply.github.com>
2022-10-07 11:48:48 +0200

    finished hover effect, need random color generator

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-const container = document.querySelector('#container');
+const mainContainer = document.querySelector('.main-container');
 
-for (i = 1; i <= 16; i++) {
-    const columnDiv = document.createElement('div');
-    container.appendChild(columnDiv).classList.add('gridRow');
+function randomColorGenerator() {
+   
 }
 
-
+for (i = 1; i <= 16; i++) {
+    const container = document.createElement('div');
+    mainContainer.appendChild(container).classList.add('container');
+    for(j = 1; j <= 16; j++) {
+        const grid = document.createElement('div');
+        container.appendChild(grid);
+        grid.classList.add('grid');
+        grid.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "purple";
+        })
+    }
+}
 
 
 
