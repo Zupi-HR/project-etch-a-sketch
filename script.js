0541e14c0aeebd903cf84731097da16b8a590868
Zupi-HR <49796750+Zupi-HR@users.noreply.github.com>
2022-10-07 10:40:08 +0200

    created 16 row divs
    need to figure how to go to the new line and create 16 divs again

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('#container');
+
+for (i = 1; i <= 16; i++) {
+    const columnDiv = document.createElement('div');
+    container.appendChild(columnDiv).classList.add('gridRow');
+}
+
+
+
+
+
